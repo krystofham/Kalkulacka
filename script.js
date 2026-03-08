06fe7bd4151250076b1c63eb3fa0174901ec0df1
krystofham <krystof.ham@volny.cz>
2026-03-08 07:49:34 +0100

    Javascrip initials

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 // Get values
+let format = [];
+const btn = document.querySelectorAll("#number");
+for (let a=0; a < 9; a++){
+    btn[a].addEventListener("click", getvalue)
+}
 
+function getvalue(btn){
+    const control = btn.target;
+    const number = parseInt(control.id.replace("n", ""));
+    format.push(number);
+    showvalues();
+}
 // Print values
-
+function showvalues(){
+    const display = document.querySelector("display");
+    display.textContent = format.join('');
+}
 // Count
+function count(a, b, o){
+    switch (o){
+        case `+`: return a+b; break;
+        case `-`: return a-b; break;
+        case `*`: return a*b; break;
+        case `/`: return a/b; break;
+    }
+}
