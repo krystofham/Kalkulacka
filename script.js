e16762c65f4d7174e4ae83240a23b40d35035eb9
krystofham <krystof.ham@volny.cz>
2026-03-08 08:33:36 +0100

    Basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Get values
 let format = [];
-const btn = document.querySelectorAll("#number");
+let second = false;
+let numbers = [];
+const btn = document.querySelectorAll(".number");
 for (let a=0; a < 9; a++){
     btn[a].addEventListener("click", getvalue)
 }
@@ -13,15 +15,51 @@ function getvalue(btn){
 }
 // Print values
 function showvalues(){
-    const display = document.querySelector("display");
+    const display = document.querySelector(".display");
     display.textContent = format.join('');
 }
 // Count
 function count(a, b, o){
     switch (o){
-        case `+`: return a+b; break;
-        case `-`: return a-b; break;
-        case `*`: return a*b; break;
-        case `/`: return a/b; break;
+        case `+`: return parseInt(a)+parseInt(b); break;
+        case `-`: return parseInt(a)-parseInt(b); break;
+        case `*`: return parseInt(a)*parseInt(b); break;
+        case `/`: return parseInt(a)/parseInt(b); break;
     }
+}
+// operators
+const operators = document.querySelectorAll(".operation");
+function operation(button){
+    numbers.push(format.join(''));
+    numbers.push(button.target.id.replace("n", ""));
+    format.length = 0;
+    second = true;
+}
+for (let a of operators){
+    a.addEventListener("click", operation);
+}
+
+//equals
+const equal = document.querySelector(".equal");
+equal.addEventListener("click", final)
+function final(){
+    numbers.push(format.join(''));
+    let num = count(numbers[0], numbers[2], numbers[1]);
+    numbers.length = 0
+
+    numbers.push(num);
+    format.length = 0
+
+    format.push(num)
+    second = false
+    showvalues() 
+}
+// C
+const c = document.querySelector(".c");
+c.addEventListener("click",  reset)
+function reset(){
+    numbers.length = 0
+    format.length = 0
+    second = false;
+    showvalues();
 }
